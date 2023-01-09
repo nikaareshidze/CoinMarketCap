9c4c437705f2d5147297f0b22419b59d033f3922
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-01-09 22:22:23 +0400

    Make Header component Dark mode tolerant

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,9 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import HeaderNavbar from "./HeaderNavbar";
 import Logo from "./Logo";
+import { darkModeActions } from "../../store/darkMode";
 
 export default function Header() {
+  const { isDark } = useSelector((state) => state.darkModeSlice);
+
   return (
-    <div className="px-[15px] py-[5px] bg-white w-full border-b border-b-gray-300">
+    <div
+      className={`px-[15px] py-[5px] w-full border-b ${
+        isDark
+          ? "bg-[#17171a] text-white border-black"
+          : "bg-white border-b-gray-300"
+      }`}
+    >
       <div className="mx-[20px] flex items-center h-[60px]">
         <Logo />
         <HeaderNavbar />
